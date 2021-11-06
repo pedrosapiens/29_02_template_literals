e3ab1de381746b5fd2a0f7328570fed0f68e800d
pedrosapiens <pedro@sapienslearning.com>
2021-11-06 12:27:18 +0100

    initial

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Template literals
+
+let nombre = 'Laura';
+let apellidos = 'Gómez Pérez';
+let resultado = 9.6;
+
+let mensaje = `La jugadora ${nombre} ${apellidos}
+tiene un resultado de ${resultado}`;
+
+console.log(mensaje);
